9b4ea5b686f639a9e09b6e78f3d416ea8a18a429
Tushar <tm33907822gmail.com>
2025-11-06 15:57:58 +0530

    Completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './contexts/CartContext.jsx'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./contexts/CartContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* We wrap our whole app in the Router (for pages) and CartProvider (for state) */}
     <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
