406633a389f64b43ce19c8917af3c5c68e6fe945
Tushar <tm33907822gmail.com>
2025-11-06 13:23:18 +0530

    All endpoints created

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -145,6 +145,7 @@ router.post('/checkout', async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Error processing checkout', error: err.message });
   }
-});
+}
+);
 
 export default router;
